051380cedaa1a35058d0dcb46696d234f92f68ec
telkomdev-dwi <dwi2warsono@gmail.com>
2018-11-17 16:13:21 +0700

    Add app.js & folder boilerplate

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var path = require("path");
+
+// Initial App
+var app = express();
+
+// View engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Setup public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Setup server
+var port = 3000;
+app.listen(port, function() {
+  console.log("Server running on port " + port);
+});
